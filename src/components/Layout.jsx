2196c2fa92588fb1d57a1b69abad532efe227809
Joe Mahlow <joe@asapcreditrepair.com>
2026-06-27 08:19:18 -0600

    Layout: add missing useEffect import - fixes app-wide blank page crash

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet, NavLink, useNavigate } from 'react-router-dom';
 import { useApp } from '../context/AppContext';
 import FloatingTools from './FloatingTools';
